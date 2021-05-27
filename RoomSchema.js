68d3c9a6a6788fc24eb76260dce99c7249a66b30
Tejas Pradhan <pradhan.tejas135@gmail.com>
2021-05-27 20:02:06 +0530

    completed room database creation

--- RoomSchema.js
+++ RoomSchema.js
@@ -1,3 +1,4 @@
+const mongoose = require("mongoose");
 var room_schema = new mongoose.Schema({
   name: String,
   secretKey: String,
@@ -6,3 +7,10 @@ var room_schema = new mongoose.Schema({
 var Room = mongoose.model("room", room_schema);
 
 module.exports = Room;
+
+// "cybersec12345"
+// "algo12345"
+// "ds12345"
+// "os12345"
+//"ai12345"
+// "se12345"
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const path = require("path");
+const mongoose = require("mongoose");
 const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
 const formatMessage = require("./utils/messages");
 const { encrypt, decrypt } = require("./utils/cryptography.js");
 const Cryptr = require("cryptr");
+const Room = require("./RoomSchema");
+const bcrypt = require("bcrypt");
 const cryptr = new Cryptr(
   "56dce7276d2b0a24e032beedf0473d743dbacf92aafe898e5a0f8d9898c9eae80a73798beed53489e8dbfd94191c1f28dc58cad12321d8150b93a2e092a744265fd214d7c2ef079e2f01b6d06319b7b2"
 );
@@ -107,7 +110,29 @@ app.get("/encrypt", (req, res) => {
   res.json(encrypted);
 });
 
-app.get("/insert", (req, res) => {});
+app.get("/insert", async (req, res) => {
+  const salt = await bcrypt.genSalt(10);
+  hashed1 = await bcrypt.hash("cybersec12345", salt);
+  hashed2 = await bcrypt.hash("algo12345", salt);
+  hashed3 = await bcrypt.hash("ds12345", salt);
+  hashed4 = await bcrypt.hash("os12345", salt);
+  hashed5 = await bcrypt.hash("ai12345", salt);
+  hashed6 = await bcrypt.hash("se12345", salt);
+
+  rooms = [
+    { name: "Cyber Security", secretKey: hashed1 },
+    { name: "Algorithms", secretKey: hashed2 },
+    { name: "Data Science", secretKey: hashed3 },
+    { name: "Operating Systems", secretKey: hashed4 },
+    { name: "Artificial Intelligence", secretKey: hashed5 },
+    { name: "Software Engineering", secretKey: hashed6 },
+  ];
+  console.log(rooms);
+  Room.insertMany(rooms, function (err, res) {
+    if (err) throw err;
+  });
+  res.send("Inserted Successfully");
+});
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
