3cfb7a6ef6597bd4a42c1f7d9e1afaeabb5833d7
Tejas Pradhan <pradhan.tejas135@gmail.com>
2021-05-27 17:44:18 +0530

    added cryptography files

---
+++ utils/cryptography.js
@@ -0,0 +1,20 @@
+const Cryptr = require('cryptr');
+const cryptr = new Cryptr('myTotalySecretKey');
+
+function encrypt(msg){
+    const encryptedString = cryptr.encrypt(msg);
+    console.log(encryptedString);
+    return encryptedString;
+}
+
+function decrypt(encryptedString){
+    console.log(encryptedString);
+    const decryptedString = cryptr.decrypt(encryptedString);
+    console.log(decryptedString);
+    return decryptedString;
+}
+
+
+ module.exports ={encrypt,decrypt};   
+    
+
